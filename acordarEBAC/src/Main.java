b027a6b4e29bfd96ffcedfd51e735687c19248ca
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2023-12-31 17:14:45 -0300

    Add files via upload

---
+++ acordarEBAC/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Ao acordar," +
+                        " tirar as cobertas de cima," +
+                        " virar e se levantar da cama ," +
+                        " calçar os chionelos, caminhar ate o banheiro," +
+                        " ecovar os dentes, lavar o rosto ," +
+                        " voltar para o quarto , se despir," +
+                        " colocar a roupa denominda pra o trabalho ," +
+                        " dobrar as cobertas da cama, caminhar ate a porta," +
+                        " colocar a chave na porta e abrila," +
+                        " sair de casa," +
+                        " colocar a chave na porta," +
+                        " virar ate fechar a porta ," +
+                        " fim    "  );
+
+
+    }
+}
