2949d04ae0d8219b99974e8f6b5b9330cc0ebee2
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2024-01-04 19:11:22 -0300

    Add files via upload

---
+++ exerciciomod7/src/Mesa.java
@@ -0,0 +1,33 @@
+// mesa moderna de sala pensada para a questao //
+public class Mesa {
+
+    private String  Material;
+
+    private String ToalhaDeMesa;
+
+    private int Cadeira;
+
+    public java.lang.String getMaterial() {
+        return Material;
+    }
+
+    public void setMaterial(java.lang.String material) {
+        Material = material;
+    }
+
+    public java.lang.String getToalhaDeMesa() {
+        return ToalhaDeMesa;
+    }
+
+    public void setToalhaDeMesa(java.lang.String toalhaDeMesa) {
+        ToalhaDeMesa = toalhaDeMesa;
+    }
+
+    public int getCadeira() {
+        return Cadeira;
+    }
+
+    public void setCadeira(int cadeira) {
+        Cadeira = cadeira;
+    }
+}
