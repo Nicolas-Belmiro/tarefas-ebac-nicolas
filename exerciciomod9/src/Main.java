d42c515029c742f524f10d729e4230f1d079797c
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2024-01-13 12:44:26 -0300

    Add files via upload

---
+++ exerciciomod9/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+         long cpf = 12345678901L;
+
+        Long cpf2 = cpf;
+
+        System.out.println(String.valueOf(cpf2));
+
+
+
+    }
+
+
+    }
