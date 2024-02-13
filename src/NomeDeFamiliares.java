82442a6fca29710a26e15db8859ca924b869c086
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2024-02-13 16:09:28 -0300

    Add files via upload

---
+++ src/NomeDeFamiliares.java
@@ -0,0 +1,54 @@
+
+import javax.swing.*;
+import java.util.*;
+
+public class NomeDeFamiliares {
+
+    private Object String;
+
+    public NomeDeFamiliares() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Primeira Parte");
+        List<String> list = new ArrayList<>();
+        list.add("Simone Elza");
+        list.add("Marcio Dias");
+        list.add("Nicolas Belmiro");
+        list.add("Hilda Lopes");
+        list.add("Yan Lucas");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println();
+
+
+    }
+
+
+    public void[] NomeDeFamiliaresSe(boolean String) {
+        return new void[0];
+    }
+
+    {
+
+        List<Comparator<NomeDeFamiliaresSe>> comparators = new ArrayList<>();
+        comparators.add(sortByArea);
+        comparators.add(sortBySex);
+
+
+            System.out.println("Segunda Parte");
+            List<String> list = new ArrayList<>();
+            list.add("Simone Elza - F");
+            list.add("Marcio Dias - M");
+            list.add("Nicolas Belmiro - M");
+            list.add("Hilda Lopes - F");
+            list.add("Yan Lucas -M");
+            System.out.println(list);
+            Collections.sort(list);
+            System.out.println(list);
+            System.out.println();
+        }
+
+
+    }
