311f029566b7d64f5e9099971411d540f7d4fb37
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2024-05-30 12:37:44 -0300

    Add files via upload

---
+++ Modulo 22/src/ListaNomes.java
@@ -0,0 +1,21 @@
+
+public class ListaNomes  {
+
+    String nome;
+
+    String genero;
+
+    public ListaNomes( String nome, String genero ){
+        this.nome= nome;
+        this.genero = genero;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+}
---
+++ Modulo 22/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<ListaNomes> listaNomes = Arrays.asList(
+                new ListaNomes("Fernanda ", " Mulher "),
+                new ListaNomes("Cezar ", " Homen "),
+                new ListaNomes("Junior ", " Homen "),
+                new ListaNomes("Marcio ", " Homen "),
+                new ListaNomes("Nicolas ", " Homen "),
+                new ListaNomes("Yan Lucas ", " Homen "),
+                new ListaNomes("Paula ", " Mulher "),
+                new ListaNomes("Hilda  ", " Mulher "),
+                new ListaNomes("Simone ", " Mulher "),
+                new ListaNomes("Marceline ", " Mulher ")
+        );
+
+//filtrar apenas nomes de mulher na lista
+        List<ListaNomes> Mulheres = listaNomes.stream()
+                .filter(ListaNomes -> ListaNomes.getGenero().equalsIgnoreCase(" Mulher "))
+                .collect(Collectors.toList());
+// imprimir lista
+        // Imprimir a lista de mulheres
+        System.out.println("Lista com Nomes de  Mulheres:");
+        Mulheres.forEach(listaNome -> System.out.println(listaNome.getNome()));
+
+
+    }
+}
+
