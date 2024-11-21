20972a443bc709799beca0848084149f6a08e727
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2024-11-21 19:59:03 -0300

    Delete Modulo 42/demo directory

--- Modulo 42/demo/src/main/java/dtos/SubscriptionRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package dtos;
-
-public record SubscriptionRequestDTO (String participantEmail) {
-
-}
--- Modulo 42/demo/src/main/java/repositories/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-
-import domain.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-
-}
